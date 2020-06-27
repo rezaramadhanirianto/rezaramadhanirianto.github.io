94e5ccb663a800578cbcaf26a5b0ce46fb9bb882
RezaRamadhanIrianto <rezaramadhanirianto@gmail.com>
2020-06-27 22:47:17 +0700

    Portfolio

--- js/custom.js
+++ js/custom.js
@@ -44,6 +44,134 @@
           }
       }
     });
+    $('#project-slide2').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          768:{
+              items:2,
+          }
+      }
+    });
+    $('#project-slide3').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          768:{
+              items:2,
+          }
+      }
+    });
+    $('#project-slide4').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          768:{
+              items:2,
+          }
+      }
+    });
+    $('#project-slide5').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          768:{
+              items:2,
+          }
+      }
+    });
+    $('#project-slide6').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          768:{
+              items:2,
+          }
+      }
+    });
+    $('#project-slide7').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          768:{
+              items:2,
+          }
+      }
+    });
+    $('#project-slide8').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          768:{
+              items:2,
+          }
+      }
+    });
+    $('#project-slide9').owlCarousel({
+      loop: true,
+      center: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      margin: 30,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          768:{
+              items:2,
+          }
+      }
+    });
 
   });
 
